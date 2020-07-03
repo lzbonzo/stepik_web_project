c1c037058b33d0d983fe5f4bef4e42fe58761198
lzbonzo <66182079+lzbonzo@users.noreply.github.com>
2020-07-03 22:13:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+#!/bin/bash
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/guni.conf /etc/gunicorn.d/django
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
